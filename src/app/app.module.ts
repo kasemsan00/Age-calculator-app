40633a1f5389e452a86b8d453740f2f619904834
kasemsan00 <106854077+kasemsan00@users.noreply.github.com>
2023-04-02 08:37:29 +0700

    dev

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InputGroupComponent } from './input-group/input-group.component';
+import { ViewGroupComponent } from './view-group/view-group.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputGroupComponent,
+    ViewGroupComponent
   ],
   imports: [
     BrowserModule,
